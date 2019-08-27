4c4d0937054cabb6af9f60064d8d162ac772b207
Geraldo Souza <soghwu@bpbiocombustiveis.local>
2019-08-27 10:47:22 -0300

    Prints

--- Screenshot_6_Caminhoes_Atraso.bat
+++ Screenshot_6_Caminhoes_Atraso.bat
@@ -1,3 +1,3 @@
 @echo off
-start firefox http://simplefarm.bpbio.com.br/#/Home/Main?panelId=132
+start chrome http://simplefarm.bpbio.com.br/#/Home/Main?panelId=132
 start /min nircmd.exe cmdwait 30000 savescreenshot "C:\Users\soghwu\Desktop\reports\prints\Atraso_TRP.png"
--- Screenshot_6_Hora_Hora.bat
+++ Screenshot_6_Hora_Hora.bat
@@ -1,4 +1,4 @@
 @echo off
-start firefox http://simplefarm.bpbio.com.br/#/Home/Main?panelId=108
+start chrome http://simplefarm.bpbio.com.br/#/Home/Main?panelId=108
 start /min nircmd.exe cmdwait 60000 savescreenshot "C:\Users\soghwu\Desktop\reports\prints\Hora_Hora_TRP.png"
 
