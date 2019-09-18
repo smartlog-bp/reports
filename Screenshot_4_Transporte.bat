bc0ef7a059640d15adc4c241961f94495479b6b3
Geraldo Souza <soghwu@bpbiocombustiveis.local>
2019-09-18 15:46:59 -0300

    Prints

--- Screenshot_4_Transporte.bat
+++ Screenshot_4_Transporte.bat
@@ -1,3 +1,3 @@
 @echo off
 start chrome http://simplefarm.bpbio.com.br/#/Home/Main?panelId=104
-start /min nircmd.exe cmdwait 60000 savescreenshot "D:\Apps\BOT_SIMPLEVIEW\reports\prints\Transporte_ITB.png"
+start /min nircmd.exe cmdwait 40000 savescreenshot "D:\Apps\BOT_SIMPLEVIEW\reports\prints\Transporte_ITB.png"
