53ffa9adef071cc027b31e80f763a81785bae6f6
Souza <geraldo.souza@bpbungebio.com.br>
2020-06-02 14:58:43 -0300

    Prints

---
+++ rec_moa_ITT.bat
@@ -0,0 +1,3 @@
+@echo off
+start chrome http://simplefarm.bpbio.com.br/#/Home/Main?panelId=157
+start /min nircmd.exe cmdwait 20000 savescreenshot "D:\Apps\BOT_SIMPLEVIEW\reports\prints\rec_moa_ITT.png"
