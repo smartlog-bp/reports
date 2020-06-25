dd917820ffa9c7c671732f109d19e7ad307800ac
smartlog-git2 <geraldo.souza@bpbungebio.com.br>
2020-06-25 09:34:04 -0300

    Prints

--- rec_moa_ITT - Copy.bat
+++ rec_moa_ITT - Copy.bat
@@ -1,4 +1,9 @@
-@echo off
+
+
+Dim wShell
+Set wShell = CreateObject("WScript.Shell")
+wShell.CurrentDirectory = "D:\Apps\BOT_SIMPLEVIEW\reports"
+
 myDate = Day(Now)
 myDate2 = Month(Now)
 myDate3 = Year(Now)
@@ -6,5 +11,6 @@ myDate4 = Hour(Now)
 myDate5 = Minute(Now)
 myDate6 = Second(Now)
 
+@echo off 
 start chrome http://simplefarm.bpbio.com.br/#/Home/Main?panelId=157
-start /min nircmd.exe cmdwait 20000 savescreenshot "D:\Apps\BOT_SIMPLEVIEW\reports\prints_qas\rec_moa_ITT.png"& myDate5 & myDate4 & myDate & myDate2
+start /min nircmd.exe cmdwait 20000 savescreenshot ""D:\Apps\BOT_SIMPLEVIEW\reports\prints_qas"&"rec_moa_ITT & myDate5 & myDate4 & myDate & myDate2.png""
