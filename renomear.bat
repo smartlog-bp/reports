4c8225bb9636188580dde2fcf66cf029b6935c40
smartlog-git2 <geraldo.souza@bpbungebio.com.br>
2020-06-30 08:34:04 -0300

    Prints

--- renomear.bat
+++ renomear.bat
@@ -1,1 +1,12 @@
-RENAME D:\Apps\BOT_SIMPLEVIEW\reports\prints_qas\rec_moa_ITT_qas.png %date:~-4,4%%date:~-7,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%.png
+@echo off
+set hora_atual=%time:~0,2%
+rem gtr significa maior
+if %hora_atual% gtr 9 goto 
+ RENAME D:\Apps\BOT_SIMPLEVIEW\reports\prints_qas\rec_moa_ITT_qas.png %date:~-4,4%%date:~-7,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%.png
+rem Acertando a hora com 0 caso seja menor que 9
+set hora_atual=0%time:~1,1%
+
+set min_atual=%time:~3,2%
+
+echo 
+ RENAME D:\Apps\BOT_SIMPLEVIEW\reports\prints_qas\rec_moa_ITT_qas.png %date:~-4,4%%date:~-7,2%%date:~0,2%%hora_atual%%min_atual%00.png 
---
+++ renomear.old.bat
@@ -0,0 +1,1 @@
+RENAME D:\Apps\BOT_SIMPLEVIEW\reports\prints_qas\rec_moa_ITT_qas.png %date:~-4,4%%date:~-7,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%.png
